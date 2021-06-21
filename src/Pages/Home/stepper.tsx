c7eadbd9bf119a82c18263cc5aaeec870f76af21
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-06-21 09:48:20 -0500

    [ Formik - Stepper ] - implement logic to go next and back between steps

--- src/Pages/Home/stepper.tsx
+++ src/Pages/Home/stepper.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@material-ui/core';
 import { Form, Formik, FormikConfig, FormikValues } from 'formik';
 import { Children, useState } from 'react';
 import { useStyle } from './styles';
@@ -8,10 +9,23 @@ const FormikStepper = ({ children, ...props }: FormikConfig<FormikValues>) => {
 	const currentChild = childrenArray[step];
 	const classes = useStyle();
 
+	const isLastStep = () => step === childrenArray.length - 1;
+
 	return (
-		<Formik {...props}>
+		<Formik
+			{...props}
+			onSubmit={async (values, helpers) => {
+				if (isLastStep()) {
+					await props.onSubmit(values, helpers);
+				} else {
+					setStep(s => s + 1);
+				}
+			}}
+		>
 			<Form autoComplete='off' className={classes.form}>
 				{currentChild}
+				{step > 0 && <Button onClick={() => setStep(s => s - 1)}>Back</Button>}
+				<Button type='submit'>Next</Button>
 			</Form>
 		</Formik>
 	);
